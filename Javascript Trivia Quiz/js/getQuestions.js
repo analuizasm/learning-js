e6aea6edd5df80473904c0433af82a7391d08f3c
ana <anaslmagalhaes@gmail.com>
2021-01-22 01:43:29 -0300

    removing files

--- Javascript Trivia Quiz/js/getQuestions.js
+++
@@ -1,8 +0,0 @@
-var startButton = document.getElementById("start-quiz");
-startButton.addEventListener('click', sendApiRequest);
-
-async function sendApiRequest(){
-    let response = await fetch(`https://opentdb.com/api.php?amount=10&category=18&type=multiple`);
-    let questions = await response.json();
-    setQuestionsArray(questions);
-};
